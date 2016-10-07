01966d5edcea34c46ae853ed202a2c343a17aa8b
umaira.sajjad@rolustech.net <umaira.sajjad@rolustech.net>
2016-10-07 14:32:18 +0500

    pluggin done, callbacks remain

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-(function() {
+(function($) {
 
-	this.Trello = function() {
+	$.fn.Trello = function() {
 		var divs = 0;
 		var cards = 0;
 		// var options = {};
@@ -19,13 +19,16 @@
 		// Create options by extending defaults with the passed in arugments
 		if (arguments[0] && typeof arguments[0] === "object") {
 			this.options = extendDefaults(defaults, arguments[0]);
-			// console.log('options', this.options);
 		}
-	}
+		var that = this;
+		document.getElementById("trigger").addEventListener("click", function(){
+			init.call(that);
+		});
+	};
 
 	// public methods
 
-	Trello.prototype.init = function () {
+	function init() {
 		// overlay div
 		var div_o = document.createElement("div");
 		div_o.className = "overlay";
@@ -36,7 +39,6 @@
 		div_o.appendChild(div_f);
 
 		var label = document.createElement("label");
-		label.style.color = "white";
 		label.innerHTML = "Title";
 
 		var input_t = document.createElement("input");
@@ -45,8 +47,6 @@
 		input_t.className = "form-control";
 
 		var button = document.createElement("button");
-		button.style.marginTop = "10px";
-		button.style.marginLeft = "100px";
 		button.className = "btn btn-success";
 		button.type = "submit";
 		button.innerHTML = "Submit";
@@ -56,10 +56,7 @@
 		button.onclick = function() {	create_board.call()};
 
 		var image = document.createElement("img");
-		image.style.marginTop = "10px";
 		image.src = "https://www.pledgesports.org/wp-content/themes/pledge_v2.3/img/close.png";
-		image.style.width = "50px";
-		image.style.height = "30px";
 		image.onclick = function() {  $(".overlay").toggle(); };
 
 		div_f.appendChild(label);
@@ -69,27 +66,21 @@
 
 		$(".overlay").toggle();
 
-		// board div
+		// board, lists div
 		var div_b = document.createElement("div");
 		div_b.id = "board";
 		div_b.style.display = "none";
+		document.body.appendChild(div_b);
 
 		var h2 = document.createElement("h2");
 
 		var input_l = document.createElement("input");
 		input_l.type = "text";
 		input_l.id = "list";
-		input_l.className = "form-control";
 		input_l.placeholder = "Add a list...";
-		input_l.style.width = "150px";
 
 		var div_d = document.createElement("div");
-		div_d.style.display = "none";
-		div_d.id = "dropdown";
-
-		document.body.appendChild(div_b);
-		div_b.appendChild(h2);
-		div_b.appendChild(input_l);
+		div_d.className = "dropdown";
 
 		var button2 = document.createElement("button");
 		button2.className = "btn btn-success";
@@ -98,16 +89,17 @@
 		button2.innerHTML = "Save";
 
 		var image2 = document.createElement("img");
-		image2.style.width = "30px";
-		image2.style.height = "30px";
 		image2.src = "https://cdn0.iconfinder.com/data/icons/slim-square-icons-basics/100/basics-22-128.png";
 		image2.onclick = function() { please_close.call() };
-
 
 		div_d.appendChild(button2);
 		div_d.appendChild(image2);
 
+		div_b.appendChild(h2);
+		div_b.appendChild(input_l);
 		div_b.appendChild(div_d);
+		$(".dropdown").toggle();
+
 	};
 
 	function create_board() {
@@ -119,13 +111,13 @@
 		$("#board").toggle();
 
 		document.getElementById("list").addEventListener("click", function(){
-			$("#dropdown").toggle();
+			$(".dropdown").toggle();
 		});
 	}
 
 	function create_list() {
 		var list_name = document.getElementById('list').value;
-		$("#dropdown").toggle();
+		$(".dropdown").toggle();
 		document.getElementById('list').value = '';
 
 		// create a list on the right
@@ -134,61 +126,14 @@
 
 		if (this.divs >= 1) {
 			this.divs = this.divs + 1;
-			//  console.log('divs2', this.divs);
 		} else {
 			this.divs = 1;
-			//  console.log('divs1', this.divs);
 		}
 		div.id = "draggable" + this.divs;
-		// console.log('divs', this.divs);
-		// console.log('options', this.options);
 		div.innerHTML = list_name;
 
 		document.body.appendChild(div);
-		if (this.options.list.axis && this.options.list.containment && this.options.list.cursor) {
-			console.log('div1');
-			$("#draggable" + this.divs).draggable({
-				axis: this.options.list.axis,
-				containment: this.options.list.containment,
-				cursor:  this.options.list.cursor
-			});
-		} else if (this.options.list.axis && !this.options.list.containment &&  !this.options.list.cursor) {
-			console.log('div2');
-			$("#draggable" + this.divs).draggable({
-				axis: this.options.list.axis
-			});
-		} else if (this.options.list.axis && !this.options.list.containment && !this.options.list.cursor) {
-			console.log('div3');
-			$("#draggable" + this.divs).draggable({
-				axis: this.options.list.axis
-			});
-		} else if (this.options.list.axis && this.options.list.containment && !this.options.list.cursor) {
-				console.log('div4');
-				$("#draggable" + this.divs).draggable({
-					axis: this.options.list.axis,
-					containment:  this.options.list.containment
-				});
-		} else if (this.options.list.axis && !this.options.list.containment && this.options.list.cursor) {
-			console.log('div5');
-			$("#draggable" + this.divs).draggable({
-				axis: this.options.list.axis,
-				cursor:  this.options.list.cursor
-			});
-		} else if (!this.options.list.axis && this.options.list.containment && this.options.list.cursor) {
-			console.log('div6');
-			$("#draggable" + this.divs).draggable({
-				containment: this.options.list.containment,
-				cursor: this.options.list.cursor
-			});
-		} else if (!this.options.list.axis && this.options.list.containment && !this.options.list.cursor) {
-			console.log('div7');
-			$("#draggable" + this.divs).draggable({
-				containment: this.options.list.containment
-			});
-		} else if (!this.options.list.axis && !this.options.list.containment && !this.options.list.cursor) {
-			console.log('div8');
-			$("#draggable" + this.divs).draggable();
-		}
+		$("#draggable" + this.divs).draggable(this.options.list);
 
 		// create input
 		var input = document.createElement("input");
@@ -196,18 +141,12 @@
 		input.id = "input" + this.divs;
 		input.class = "form-control";
 		input.placeholder = "Add a card...";
-		input.style.width = "120px";
 		div.appendChild(input);
 
 		$("#" + input.id).droppable({
 			drop: function (event, ui) {
-				//  console.log('this', $(this).parent()[0]);
-				// $(this).closest('div').after(ui.helper[0]);
-				//  console.log('dropped!', ui.helper[0]);
 				var child = ui.helper[0];
 				var parent = $(this).parent()[0];
-				// parent.style.overflow = "hidden";
-				// parent.style.width = "100%";
 				child.style.left = "0px";
 				child.style.top = "0px";
 
@@ -228,8 +167,7 @@
 
 		// create a dropdown div
 		var	div_d = document.createElement("div");
-		div_d.style.width = "150px";
-		div_d.style.height = "30px";
+		div_d.className = "dropdown";
 		div_d.id = "dropdown" + this.divs;
 		div.appendChild(div_d);
 
@@ -245,8 +183,6 @@
 		// console.log('that.divs', that.divs);
 		var image = document.createElement("img");
 		image.src = "https://cdn0.iconfinder.com/data/icons/slim-square-icons-basics/100/basics-22-128.png";
-		image.style.width = "30px";
-		image.style.height = "30px";
 		image.onclick = function() { please_close2(that.divs) };
 
 
@@ -293,64 +229,12 @@
 		parent_div.appendChild(text_area);
 
 		// console.log('options111', this.options);
-		if (this.options.card.axis && this.options.card.containment && this.options.card.cursor) {
-			console.log('div1');
-			$("#" + text_area.id).draggable({
-				cancel: "",
-				axis: this.options.card.axis,
-				containment: this.options.card.containment,
-				cursor:  this.options.card.cursor
-			});
-		} else if (this.options.card.axis && !this.options.card.containment &&  !this.options.card.cursor) {
-			console.log('div2');
-			$("#" + text_area.id).draggable({
-				cancel: "",
-				axis: this.options.card.axis
-			});
-		} else if (this.options.card.axis && this.options.card.containment && !this.options.card.cursor) {
-			console.log('div3');
-			$("#" + text_area.id).draggable({
-				cancel: "",
-				axis: this.options.card.axis,
-				containment: this.options.card.containment
-			});
-		} else if (this.options.card.axis && !this.options.card.containment && this.options.card.cursor) {
-				console.log('div4');
-				$("#" + text_area.id).draggable({
-					cancel: "",
-					axis: this.options.card.axis,
-					cursor:  this.options.card.cursor
-				});
-		} else if (!this.options.card.axis && this.options.card.containment && this.options.card.cursor) {
-			console.log('div5');
-			$("#" + text_area.id).draggable({
-				cancel: "",
-				containment: this.options.card.containment,
-				cursor:  this.options.card.cursor
-			});
-		} else if (!this.options.card.axis && this.options.card.containment && !this.options.card.cursor) {
-			console.log('div6');
-			$("#" + text_area.id).draggable({
-				cancel: "",
-				containment: this.options.card.containment
-			});
-		} else if (!this.options.card.axis && !this.options.card.containment && this.options.card.cursor) {
-			console.log('div7');
-			$("#" + text_area.id).draggable({
-				cancel: "",
-				cursor: this.options.card.cursor
-			});
-		} else if (!this.options.card.axis && !this.options.card.containment && !this.options.card.cursor) {
-			console.log('div8');
-			$("#" + text_area.id).draggable({
-				cancel: ""
-			});
-		}
-
+		this.options.card.cancel = "";
+		$("#" + text_area.id).draggable(this.options.card);
 	}
 
 	function please_close() {
-		$("#dropdown").toggle();
+		$(".dropdown").toggle();
 		document.getElementById('list').value = '';
 	}
 
@@ -370,11 +254,11 @@
 		return source;
 	}
 
-}());
 
-var trello = new Trello({
- // set list and card here
- 	list : {
+}(jQuery));
+
+$("#begin" ).Trello({
+	list : {
 		axis: "x",
 		containment: "parent",
 		cursor: "crosshair"
@@ -382,8 +266,4 @@ var trello = new Trello({
 	card: {
 		cursor: "crosshair"
 	}
-});
-// console.log('trigger', document.getElementById("trigger") );
-document.getElementById('trigger').addEventListener("click", function()		{
-	trello.init();
 });
